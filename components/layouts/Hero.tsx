37dee54547a65ab74addf1392effaca038172c5c
Saeed-Altout <saeedaltoutpro@gmail.com>
2023-11-24 10:50:17 +0300

    fix &apos;

--- components/layouts/Hero.tsx
+++ components/layouts/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
             Crafting Innovative Digital Solutions
           </h1>
           <p className="leading-8 font-medium text-neutral-600">
-            At MicroTech, we're dedicated to transforming your ideas into
+            At MicroTech, we&apos;re dedicated to transforming your ideas into
             powerful digital experiences. As a leading application design and
             development company, we specialize in creating customized,
             cutting-edge solutions that propel your business forward. Discover
